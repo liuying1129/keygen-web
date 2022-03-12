d83d40d8f93662373890376ee75f9905e6397663
1 <1>
2022-03-12 20:34:09 +0800

    目录初始化

---
+++ src/main/java/com/yklis/keygenweb/KeygenWebApplication.java
@@ -0,0 +1,13 @@
+package com.yklis.keygenweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeygenWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeygenWebApplication.class, args);
+	}
+
+}
